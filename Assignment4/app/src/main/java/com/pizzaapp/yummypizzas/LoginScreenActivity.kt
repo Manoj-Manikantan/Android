fed8d5b4f785de4f53a83792c1692472a7ae1ffc
Manoj <manojmkt127@gmail.com>
2020-11-21 18:35:43 -0500

    feat: login screen layout added

---
+++ Assignment4/app/src/main/java/com/pizzaapp/yummypizzas/LoginScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.pizzaapp.yummypizzas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_screen)
+    }
+}
