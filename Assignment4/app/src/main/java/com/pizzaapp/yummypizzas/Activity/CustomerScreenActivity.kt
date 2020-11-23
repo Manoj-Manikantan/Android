dd73ece3d1caf7e7f4a34cb19f8a1dceb8f0e9e3
Manoj <manojmkt127@gmail.com>
2020-11-22 23:08:27 -0500

    fix: customer screen intents

--- Assignment4/app/src/main/java/com/pizzaapp/yummypizzas/Activity/CustomerScreenActivity.kt
+++ Assignment4/app/src/main/java/com/pizzaapp/yummypizzas/Activity/CustomerScreenActivity.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.pizzaapp.yummypizzas.Adapter.OrderListAdapter
 import com.pizzaapp.yummypizzas.R
+import com.pizzaapp.yummypizzas.Room.Database.PizzaDatabase
+import com.pizzaapp.yummypizzas.Room.Entity.Order
 import com.pizzaapp.yummypizzas.Utility.SPreference
 import kotlinx.android.synthetic.main.activity_customer_screen.*
 
@@ -25,9 +28,9 @@ class CustomerScreenActivity : AppCompatActivity() {
 
     fun onButtonPressed(view: View) {
         if (view.id == R.id.btnLogout) {
-            sPreference.setBooleanValue(SPreference.isLogin,false)
-            sPreference.setStringValue(SPreference.userName,"")
-            sPreference.setStringValue(SPreference.password,"")
+            sPreference.setBooleanValue(SPreference.isLogin, false)
+            sPreference.setStringValue(SPreference.userName, "")
+            sPreference.setStringValue(SPreference.password, "")
             Toast.makeText(this, "Logout successful", Toast.LENGTH_SHORT).show()
             startActivity(Intent(this, MainScreenActivity::class.java))
             finish()
@@ -35,13 +38,40 @@ class CustomerScreenActivity : AppCompatActivity() {
             if (sPreference.getBooleanValue(SPreference.isPersonalInfoFilled)) {
                 startActivity(Intent(this, CustomerPizzaOrderActivity::class.java))
             } else {
-                Toast.makeText(this, "Please fill all the fields in Personal information to place an order", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this,
+                    "Please fill all the fields in Personal information to place an order",
+                    Toast.LENGTH_SHORT
+                ).show()
                 startActivity(Intent(this, CustomerInfoActivity::class.java))
             }
         } else if (view.id == R.id.btnPersonalInfo) {
             startActivity(Intent(this, CustomerInfoActivity::class.java))
         } else if (view.id == R.id.btnOrderDetails) {
-            startActivity(Intent(this, CustomerOrderSummaryActivity::class.java))
+
+            if (sPreference.getBooleanValue(SPreference.isPersonalInfoFilled)) {
+                val roomDatabase = PizzaDatabase.getDatabase(this)
+                val allOrders = roomDatabase.orderDAO().getAllOrders() as ArrayList<Order>
+                if (allOrders.isNotEmpty()) {
+                    startActivity(Intent(this, CustomerOrderSummaryActivity::class.java))
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Please place your order to view the status",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    startActivity(Intent(this, CustomerPizzaOrderActivity::class.java))
+                }
+            } else {
+                Toast.makeText(
+                    this,
+                    "Please fill all the fields in Personal information to place an order",
+                    Toast.LENGTH_SHORT
+                ).show()
+                startActivity(Intent(this, CustomerInfoActivity::class.java))
+            }
+
+
         }
     }
 }
